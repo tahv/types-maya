811c27e79f67e7f158b906db928687d92d636940
Thibaud Gambier <thibaud.gambier@gmail.com>
2026-02-20 10:37:54 +0100

    Add `Incomplete` to `maya.api` classes

---
+++ src/maya-stubs/api/OpenMayaAnim.pyi
@@ -0,0 +1,29 @@
+from _typeshed import Incomplete
+
+__all__ = [
+    "MAnimControl",
+    "MAnimCurveChange",
+    "MAnimCurveClipboard",
+    "MAnimCurveClipboardItem",
+    "MAnimCurveClipboardItemArray",
+    "MAnimMessage",
+    "MAnimUtil",
+    "MFnAnimCurve",
+    "MFnGeometryFilter",
+    "MFnIkJoint",
+    "MFnSkinCluster",
+    "MFnWeightGeometryFilter",
+]
+
+MAnimControl: Incomplete
+MAnimCurveChange: Incomplete
+MAnimCurveClipboard: Incomplete
+MAnimCurveClipboardItem: Incomplete
+MAnimCurveClipboardItemArray: Incomplete
+MAnimMessage: Incomplete
+MAnimUtil: Incomplete
+MFnAnimCurve: Incomplete
+MFnGeometryFilter: Incomplete
+MFnIkJoint: Incomplete
+MFnSkinCluster: Incomplete
+MFnWeightGeometryFilter: Incomplete
